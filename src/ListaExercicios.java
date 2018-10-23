e8f6d04bbe526c2389e6f3b7d0c84acbd9937f3f
Ronaldo Neves <ronaldo@ronaldoneves.com.br>
2018-10-23 11:54:10 -0300

    Finalizando a lista de exercícios

--- src/ListaExercicios.java
+++ src/ListaExercicios.java
@@ -1,6 +1,7 @@
 import jdk.nashorn.internal.scripts.JO;
 
 import javax.swing.*;
+import java.util.Arrays;
 
 public class ListaExercicios {
 
@@ -82,6 +83,133 @@ public class ListaExercicios {
     }
     */
 
+    /* EXERCÍCIO 5
+    public static void main(String[] args){
 
+        float altura;
+        Double pesoIdeal;
+        String nome, sexo, aux;
+
+        nome = JOptionPane.showInputDialog("Informe o nome");
+        sexo = JOptionPane.showInputDialog("Informe o sexo");
+        aux = JOptionPane.showInputDialog("Informe a altura");
+        altura = Float.parseFloat(aux);
+
+        if(sexo.equals("M")){
+            pesoIdeal = (72.7 * altura) - 58;
+
+            JOptionPane.showMessageDialog(null, nome + ", seu peso ideal como homem é: " + pesoIdeal);
+        } else {
+            pesoIdeal = (62.1 * altura) - 44.7;
+            JOptionPane.showMessageDialog(null, nome + ", o seu peso ideal como mulher é: " + pesoIdeal);
+        }
+
+    }
+    */
+
+    /* EXERCÍCIO 6
+    public static void main(String[] args) {
+        int i;
+
+        for(i = 10; i >= 1; i--){
+            JOptionPane.showMessageDialog(null, i);
+        }
+    }
+    */
+
+    /* EXERCÍCIO 7
+    public static void main(String[] args){
+
+        int n, i;
+        String aux;
+
+        aux = JOptionPane.showInputDialog("Informe N");
+        n = Integer.parseInt(aux);
+
+        for(i = 1; i <= n; i++){
+            JOptionPane.showMessageDialog(null, i);
+        }
+    }
+    */
+
+    /* EXERCÍCIO 8
+    public static void main(String[] args){
+
+        int n;
+
+        for(n = 1; n <= 10; n++){
+            JOptionPane.showMessageDialog(null, "8 x " + n + " = " + (8 * n) );
+        }
+
+    }
+    */
+
+    /* EXERCÍCIO 9
+    public static void main(String[] args){
+        int n, i;
+        String aux;
+
+        aux = JOptionPane.showInputDialog("Informe o número");
+        n = Integer.parseInt(aux);
+
+        if(n > 10){
+            JOptionPane.showMessageDialog(null, "O número informado deve estar entre 1 e 10");
+            aux = JOptionPane.showInputDialog("Informe o número");
+            n = Integer.parseInt(aux);
+        }
+
+        for(i = 1; i <= 10; i++){
+            JOptionPane.showMessageDialog(null, n + " x " + i + " = " + (n * i) );
+//            System.out.println(n + " x " + i + " = " + (n * i) );
+        }
+
+    }
+    */
+
+    public static void main(String[] args) {
+
+        int soma = 0, i;
+        int[] n = new int[10];
+        String aux;
+
+        aux = JOptionPane.showInputDialog("Informe o número 1: ");
+        n[0] = Integer.parseInt(aux);
+
+        aux = JOptionPane.showInputDialog("Informe o número 2: ");
+        n[1] = Integer.parseInt(aux);
+
+        aux = JOptionPane.showInputDialog("Informe o número 3: ");
+        n[2] = Integer.parseInt(aux);
+
+        aux = JOptionPane.showInputDialog("Informe o número 4: ");
+        n[3] = Integer.parseInt(aux);
+
+        aux = JOptionPane.showInputDialog("Informe o número 5: ");
+        n[4] = Integer.parseInt(aux);
+
+        aux = JOptionPane.showInputDialog("Informe o número 6: ");
+        n[5] = Integer.parseInt(aux);
+
+        aux = JOptionPane.showInputDialog("Informe o número 7: ");
+        n[6] = Integer.parseInt(aux);
+
+        aux = JOptionPane.showInputDialog("Informe o número 8: ");
+        n[7] = Integer.parseInt(aux);
+
+        aux = JOptionPane.showInputDialog("Informe o número 9: ");
+        n[8] = Integer.parseInt(aux);
+
+        aux = JOptionPane.showInputDialog("Informe o número 10: ");
+        n[9] = Integer.parseInt(aux);
+
+        for (i = 0; i < 10; i++) {
+            if(n[i] < 40) {
+                soma += n[i];
+            }
+        }
+
+        JOptionPane.showMessageDialog(null, soma);
+        
+    }
 
 }
